3603d3f585301822dd7e686e868c29489436de45
jibgarcia <jibgarcia@gmail.com>
2022-10-20 07:14:56 +0200

    Update config

--- config.py
+++ config.py
@@ -10,12 +10,11 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 8000 #3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "fc0dc04e-60b2-4984-a6e1-f21731ca592c")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "34090205-33c9-4ccd-8154-b2e1d6775f63")
     LUIS_APP_ID = os.environ.get("LuisAppId", "1f6d13d5-e766-4367-a3c6-a6a2a789789b")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "76b1d1cfec80489680b4a804ac7c662c")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        # "AppInsightsInstrumentationKey", "5d1c1614-4149-432f-ba86-cb5dffd14d89"
         "AppInsightsInstrumentationKey", "5d1c1614-4149-432f-ba86-cb5dffd14d89"
     )
