25c7b93db3326ce7a56721991f829e8177235889
jibgarcia <jibgarcia@gmail.com>
2022-10-22 16:44:13 +0200

    Update app password

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class DefaultConfig:
     # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant") 
     APP_ID = os.environ.get("MicrosoftAppId", "fc0dc04e-60b2-4984-a6e1-f21731ca592c")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "34090205-33c9-4ccd-8154-b2e1d6775f63")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "9nG8Q~NlUDfQXd8IDvZjxIr3ngNCdLeluD2GCcY9")
     LUIS_APP_ID = os.environ.get("LuisAppId", "1f6d13d5-e766-4367-a3c6-a6a2a789789b")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "76b1d1cfec80489680b4a804ac7c662c")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
